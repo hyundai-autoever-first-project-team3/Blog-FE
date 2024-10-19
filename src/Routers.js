be6fc3a601a279740bbf6415c6af699ace866cfa
이효원 <ymj07168@gmail.com>
2024-10-19 17:58:43 +0900

    feat: 라우터 추가

---
+++ src/Routers.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import PostDetailPage from "./pages/PostDetailPage";
+import PostWritePage from "./pages/PostWritePage";
+import ChallengePage from "./pages/ChallengePage";
+import MyPage from "./pages/MyPage";
+
+const Routers = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* 메인페이지 - 전체 글 목록 / TIL 목록 */}
+        <Route element={<MainPage />} path="/" exact />
+        {/* 글 상세 페이지 */}
+        <Route element={<PostDetailPage />} path="/posts/:postId" />
+        {/* 글 작성 페이지 */}
+        <Route element={<PostWritePage />} path="/posts/write" />
+        {/* 챌리지 상세 페이지 - 일일 코딩테스트 문제 리스트 */}
+        <Route element={<ChallengePage />} path="/challenges/:challengeId" />
+        {/* 마이페이지 */}
+        <Route element={<MyPage />} path="/mypage" />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Routers;
