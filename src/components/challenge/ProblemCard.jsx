20697f1762596363f324bbb946e06fd57258f43b
chaelin2 <gcl693427@gmail.com>
2024-10-22 16:50:56 +0900

    Problem card

--- src/components/challenge/ProblemCard.jsx
+++ src/components/challenge/ProblemCard.jsx
@@ -5,28 +5,56 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMedal } from "@fortawesome/free-solid-svg-icons";
 
 const ProblemCard = () => {
-  const tags = ["백준-동전", "프로그래머스-등굣길", "LeetCode-Prefix"];
+  const tags = ["hard", "medium", "easy"];
   return (
-    <div className="flex flex-col justify-between min-h-40 h-full shadow-md rounded-md p-4 border">
-      <div className="flex flex-col ">
-        <div className="flex flex-row justify-end">
-          <FontAwesomeIcon icon={faMedal} style={{ color: "#FFD43B" }} />
+    <div className="flex min-h-30 justify-between h-full py-5 px-3 my-5 border-2 p-2 rounded-lg shadow-sm">
+      <div className="flex flex-col px-3">
+        <div className="text-xl font-semibold mb-2">경로찾기</div>
+        <div className="text-lg">백준</div>
+        <div className="text- lg">{tags[0]}</div>
+      </div>
+
+      <div className="flex flex-row items-center ">
+        <div className="flex flex-col mx-4 ">
+          <Button
+            className="my-2"
+            variant="outlined"
+            color="success"
+            style={{ borderRadius: "1rem" }}
+          >
+            질문방
+          </Button>
+          <Button
+            className="my-2"
+            variant="contained"
+            color="success"
+            style={{ borderRadius: "1rem" }}
+          >
+            도전하기
+          </Button>
         </div>
-        <div className="text-lg font-semibold">BFS/DFS 알고리즘</div>
-        <div className="flex flex-row flex-wrap gap-2">
-          {tags.map((item) => (
-            <div className="text-sm">#{item}</div>
-          ))}
+        <div className="flex flex-row justify-end px-2">
+          <FontAwesomeIcon
+            icon={faMedal}
+            style={{ color: "#FFD43B" }}
+            className="fa-4x"
+          />
         </div>
       </div>
-      <div className="flex flex-row justify-end">
-        <div>
+      {/* justify-between min-h-40 h-full shadow-md rounded-md p-4 border */}
+      {/* 아이콘 */}
+      {/* <div className="flex flex-row justify-end">
+        <FontAwesomeIcon icon={faMedal} style={{ color: "#FFD43B" }} />
+      </div> */}
+
+      {/* 버튼 */}
+      {/* <div>
           <Button
             variant="outlined"
             color="success"
             style={{ borderRadius: "1rem" }}
           >
-            챌린지방?
+            질문방
           </Button>
           <Button
             variant="contained"
@@ -35,13 +63,7 @@ const ProblemCard = () => {
           >
             도전하기
           </Button>
-        </div>
-      </div>
-
-      <div className="flex flex-col">
-        <div className="text-base">조회수 {234}</div>
-        <div className="text-base font-thin text-gray-3">2024년 10월 18일</div>
-      </div>
+        </div> */}
     </div>
   );
 };
