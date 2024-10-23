94426c95f86c62743d46d231016036b89652e059
이효원 <ymj07168@gmail.com>
2024-10-23 20:05:16 +0900

    feat: 전체 TIL 조회 api 연동

--- src/api/main.js
+++ src/api/main.js
@@ -1,11 +1,7 @@
 // import client from "./client";
 import client from "./client";
 
-
-export const getTIL = async (
-  {pageNumber}
-) => {
+// TIL 글 목록 조회
+export const getTIL = async ({ pageNumber }) => {
   return await client.get(`/api/tils?page=${pageNumber}`);
 };
-
-
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,34 +4,45 @@ import Header from "../components/common/Header";
 import TabBar from "../components/common/TabBar";
 import Card from "../components/common/Card";
 import ChallengeCard from "../components/challenge/ChallengeCard";
-import { getCookie, setCookie } from "../api/cookie";
+import { setCookie } from "../api/cookie";
+import { getTIL } from "../api/main";
 
 const MainPage = () => {
   const [token, setToken] = React.useState("");
+  const [posts, setPosts] = React.useState(null);
 
-  console.log(token);
+  console.log(posts);
 
   useEffect(() => {
-    const data = getCookie("accessToken");
-    setCookie("ddddd", "@3423");
-    setToken(data);
+    setCookie(
+      "accessToken",
+      "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoi7J207Zqo7JuQIiwic29jaWFsIjoia2FrYW8iLCJlbWFpbCI6InltajA3MTY4QG5hdmVyLmNvbSIsInByb2ZpbGVJbWFnZSI6Imh0dHA6Ly9rLmtha2FvY2RuLm5ldC9kbi9kZkFXemcvYnRzSm43bVlmZm8vYzdhZ3Frb2NZTjQ1Zjd4SEIyMXN2Sy9pbWdfNjQweDY0MC5qcGciLCJyb2xlIjoiTUVNQkVSIiwiaWF0IjoxNzI5NjgwMDIwLCJleHAiOjE3Mjk2ODM2MjB9.u2lfSAabfwXrCeuMH-lr0FAGVq4ZNM2PlqAe6qwBZ1o"
+    );
+
+    getTIL({ pageNumber: 0 }).then((res) => setPosts(res.data.content));
   }, []);
   return (
     <PageContainer>
       <Header />
       <TabBar />
+      <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-3 px-2">
+        {posts?.map((item) => (
+          <Card
+            id={item.id}
+            title={item.title}
+            content={item.content}
+            thumbnail={item.thumbnail}
+            likeCount={item.likeCount}
+            createdAt={item.createdAt}
+            writerNickname={item.writerNickname}
+          />
+        ))}
+      </div>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-2 px-2">
         {Array(9)
           .fill(0)
           .map((item) => (
             <ChallengeCard />
-          ))}
-      </div>
-      <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-3 px-2">
-        {Array(9)
-          .fill(0)
-          .map((item) => (
-            <Card />
           ))}
       </div>
     </PageContainer>
