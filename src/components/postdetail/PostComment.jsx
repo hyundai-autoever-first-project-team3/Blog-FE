c5924c309f8ed2a9c39664cecf7f64c15c7964b2
이효원 <ymj07168@gmail.com>
2024-10-24 21:23:42 +0900

    feat: 댓글 연동

--- src/components/postdetail/PostComment.jsx
+++ src/components/postdetail/PostComment.jsx
@@ -1,28 +1,35 @@
 import React from "react";
 
-const PostComment = () => {
+const PostComment = ({
+  id,
+  content,
+  createdAt,
+  updatedAt,
+  writerId,
+  nickName,
+  profile,
+}) => {
+  const formattedDate = new Date(createdAt).toLocaleDateString("ko-KR", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
   return (
-    <div>
-      <div className="flex flex-col justify-items-start my-8">
+    <>
+      <div className="flex flex-col justify-items-start my-8" key={id} id={id}>
         <div className="flex flex-row items-center mb-2 ">
-          <img
-            className="size-16 rounded-full"
-            src="https://images.unsplash.com/photo-1551963831-b3b1ca40c98e"
-            alt=""
-          />
+          <img className="size-16 rounded-full" src={profile} alt="" />
           <div className="flex flex-col px-4">
-            <div className="text-lg font-semibold ">고채린</div>
-            <div className="text-md ">2024년 10월 20일</div>
+            <div className="text-lg font-semibold ">{nickName}</div>
+            <div className="text-md ">{formattedDate}</div>
           </div>
         </div>
-        <div className="text-xl">
-          블로그 글 정말 잘 읽었습니다.글을 통해 많은 정보를 얻을 수 있었고
-          앞으로도 좋은 글 많이 기대하겠습니다!
-        </div>
+        <div className="text-xl">{content}</div>
 
         <hr className="my-4" />
       </div>
-    </div>
+    </>
   );
 };
 
--- src/pages/PostDetailPage.jsx
+++ src/pages/PostDetailPage.jsx
@@ -8,13 +8,14 @@ import Footer from "../components/postdetail/Footer";
 import { Tag } from "@mui/icons-material";
 import { setCookie } from "../api/cookie";
 import { getTILDetail } from "../api/main";
-import { getLikeCount, getlikeCount } from "../api/postdetail";
 import { useParams } from "react-router-dom";
 import PostComment from "../components/postdetail/PostComment";
 import MDEditor from "@uiw/react-md-editor/nohighlight";
+import { Button, TextField } from "@mui/material";
 
 const PostDetailPage = () => {
   const [postsDetail, setPostsDetail] = React.useState(null);
+  const [newComment, setNewComment] = React.useState("");
 
   const { postId } = useParams("postId");
 
@@ -25,7 +26,7 @@ const PostDetailPage = () => {
   return (
     <>
       <Header />
-      <PageContainer className="xl:px-[250px] lg:px-[100px] md:px-5 sm:px-3">
+      <PageContainer className="px-3 xl:px-[250px] lg:px-[100px] md:px-5 sm:px-3">
         <h1 className="text-5xl font-extrabold mb-8 mt-10 lg:mt-20">
           {postsDetail?.til.title}
         </h1>
@@ -81,12 +82,36 @@ const PostDetailPage = () => {
         <div className="flex items-center justify-center w-full my-10">
           <Pagination count={10} color="success" />
         </div>
+        <div className="flex flex-col w-full gap-2">
+          <div className="text-2xl font-semibold self-start">
+            {postsDetail?.commentCounts}개의 댓글
+          </div>
+          <TextField
+            value={newComment}
+            multiline
+            rows={5}
+            placeholder="댓글을 작성하세요"
+            onChange={(e) => setNewComment(e.target.value)}
+          />
+          <Button
+            variant="contained"
+            color="success"
+            sx={{
+              alignSelf: "end",
+            }}
+          >
+            댓글 작성
+          </Button>
+        </div>
         <div className="mt-12">
-          <PostComment />
-          <PostComment />
-          <PostComment />
-          <PostComment />
-          <PostComment />
+          {postsDetail?.commentDetailDtos.map((item) => (
+            <PostComment
+              id={postsDetail?.commentDetailDtos.id}
+              content={postsDetail?.commentDetailDtos.content}
+              nickName={postsDetail?.commentDetailDtos.writerNickname}
+              profile={postsDetail?.commentDetailDtos.writerProfileImage}
+            />
+          ))}
         </div>
       </PageContainer>
     </>
