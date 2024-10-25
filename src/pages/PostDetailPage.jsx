35a4bdb30eed700c91134f5c8b8633915fc280c8
KIM_DEAHO <102588838+DHowor1d@users.noreply.github.com>
2024-10-25 15:47:27 +0900

    viewer-light-mode

--- src/pages/PostDetailPage.jsx
+++ src/pages/PostDetailPage.jsx
@@ -80,7 +80,7 @@ const PostDetailPage = () => {
             />
           )}
         </div>
-        <div className="mt-10">
+        <div className="mt-10" data-color-mode="light">
           <MDEditor.Markdown
             source={postsDetail?.til.content}
             style={{ whiteSpace: "pre-wrap" }}
