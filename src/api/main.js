b466c93a47ec5fd68e574d07d3303a2870924054
zoonmy <pjm2571@naver.com>
2024-10-25 11:27:50 +0900

    refactor: main.js token 수정

--- src/api/main.js
+++ src/api/main.js
@@ -2,10 +2,8 @@
 import client from "./client";
 import { getCookie } from "./cookie";
 
-var token = getCookie("accessToken");
+const token = getCookie("accessToken");
 
-token =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiTUVNQkVSIiwicHJvZmlsZUltYWdlIjoiaHR0cDovL2sua2FrYW9jZG4ubmV0L2RuLzJRRlBXL2J0c2FTaklIMUdUL2tEaTF3R2JHT3VIWXBtZHNURkJjOWsvaW1nXzY0MHg2NDAuanBnIiwiZW1haWwiOiJid3MxNzQyQG5hdmVyLmNvbSIsInNvY2lhbCI6Imtha2FvIiwibmFtZSI6IuuwleykgOuvvCIsImlhdCI6MTcyOTgxNzQyMywiZXhwIjoxNzI5ODUzNDIzfQ.AViqDlUJMOKzX9_rlSGwqhmNOWv2tax9DkaXDut78IE";
 // TIL 글 목록 조회
 export const getTIL = async ({ pageNumber }) => {
   return await client.get(`/api/tils?page=${pageNumber}`, {
